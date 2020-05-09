9fd0e812041ec30517d18d48a0eeacce7a0bc28b
Oscar Yesid Cardenas Santos <oscardenas17@gmail.com>
2020-05-09 11:23:24 -0500

    CARGANDO UN .TXT CON FETCH API

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+
+
+function cargarTXT() {
+    fetch('datos.txt')
+        .then(function(res) {
+            return res.text();
+        })
+        .then(function(empleados) {
+            console.log(empleados);
+            document.getElementById('resultado').innerHTML = empleados;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
