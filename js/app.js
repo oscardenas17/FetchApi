31728a40397e7ee77cd6a88c7b7a2a4b90dbdc00
Oscar Yesid Cardenas Santos <oscardenas17@gmail.com>
2020-05-09 11:24:30 -0500

    CARGANDO UN JSON CON FETCH API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 
 
 function cargarTXT() {
@@ -15,3 +16,22 @@ function cargarTXT() {
         });
 }
 
+
+function cargarJSON() {
+    fetch('empleados.json')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado) {
+                html += `
+                    <li>${empleado.nombre} ${empleado.puesto}</li>
+                `;
+            })
+            document.getElementById('resultado').innerHTML = html;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
